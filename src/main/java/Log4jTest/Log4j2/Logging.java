735438542b14b975d3f37e1ec9d4b69c5b99f738
ankur-katiyar <ankur.katiyar@gmail.com>
2021-12-16 18:07:46 -0500

    updates

--- src/main/java/Log4jTest/Log4j2/Logging.java
+++ src/main/java/Log4jTest/Log4j2/Logging.java
@@ -32,6 +32,7 @@ public class Logging extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		System.setProperty("com.sun.jndi.ldap.object.trustURLCodebase","true");
 		logger.trace("Entering application...");
 
 		logger.info("Hello Log4j2...");
